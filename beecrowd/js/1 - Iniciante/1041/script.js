0981ca2a8a88bae7702210fa9aa62dadb1e92409
queziafiladelfo <queziafiladelfo@gmail.com>
2022-06-04 06:07:38 -0300

    1041 - Coordenadas de um Ponto

--- beecrowd/js/1 - Iniciante/1041/script.js
+++ beecrowd/js/1 - Iniciante/1041/script.js
@@ -4,12 +4,21 @@ var lines = input.split(' ');
 const x = parseFloat(lines.shift());
 const y = parseFloat(lines.shift());
 
+let origin = (x + y === 0.0); 
+let eixoX = y === 0.0;
+let eixoY = x === 0.0;
 let q1 = (x > 0) && (y > 0);
 let q2 = (x < 0) && (y > 0);
 let q3 = (x < 0) && (y < 0);
 let q4 = (x > 0) && (y < 0);
 
-if(q1){
+if (origin) {
+    console.log("Origem");
+} else if(eixoX){
+    console.log("Eixo X");
+} else if(eixoY){
+    console.log("Eixo Y");
+} else if(q1){
     console.log("Q1");
 } else if(q2){
     console.log("Q2");
@@ -17,6 +26,4 @@ if(q1){
     console.log("Q3");
 } else if(q4) {
     console.log("Q4");
-} else {
-    console.log("Origem");
-}
+} 
