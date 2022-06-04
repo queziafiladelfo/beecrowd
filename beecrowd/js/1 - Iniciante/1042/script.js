33fb7e3312742b115aa67bbb5682fa0071dd9e54
queziafiladelfo <queziafiladelfo@gmail.com>
2022-06-04 17:38:00 -0300

    1042 - Sort Simples

---
+++ beecrowd/js/1 - Iniciante/1042/script.js
@@ -0,0 +1,21 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split(' ');
+
+let valores = [];
+let valoresCrescente = [];
+
+valores = lines.map((item) => parseInt(item));
+
+valoresCrescente = [...valores];
+
+valoresCrescente.sort((a, b) =>  a - b);
+
+for (let valor of valoresCrescente) {
+    console.log(valor);
+}
+
+console.log();
+
+for (let valor of valores) {
+    console.log(valor);
+}
